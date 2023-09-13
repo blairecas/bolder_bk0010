f0e47133fceb47a3fe973b9ddf175875cb25f63b
blairecas <chaminealex@mail.ru>
2023-09-13 23:15:16 +0300

    Compile to go

--- 1_compile.bat
+++ 1_compile.bat
@@ -12,16 +12,16 @@ echo.
 echo ===========================================================================
 echo Compiling V0
 echo ===========================================================================
-..\..\php5\php.exe -c ..\..\php5\ -f ..\scripts\preprocess.php cpu.mac
+..\..\php5\php.exe -c ..\..\php5\ -f preprocess.php cpu.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\..\macro11\macro11.exe -ysl 32 -yus -m ..\..\macro11\sysmac.sml -l _cpu.lst _cpu.mac
+macro11.exe -ysl 32 -yus -m sysmac.sml -l _cpu.lst _cpu.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 
 echo.
 echo ===========================================================================
 echo Linking V0
 echo ===========================================================================
-..\..\php5\php.exe -c ..\..\php5\ -f ..\scripts\lst2bin.php _cpu.lst ./release/bolde2.bin bbk
+..\..\php5\php.exe -c ..\..\php5\ -f lst2bin.php _cpu.lst ./release/bolde2.bin bbk
 
 echo.
 echo ===========================================================================
@@ -35,15 +35,15 @@ echo.
 echo ===========================================================================
 echo Compiling V1
 echo ===========================================================================
-..\..\php5\php.exe -c ..\..\php5\ -f ..\scripts\preprocess.php cpu.mac
+..\..\php5\php.exe -c ..\..\php5\ -f preprocess.php cpu.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\..\macro11\macro11.exe -ysl 32 -yus -m ..\..\macro11\sysmac.sml -l _cpu.lst _cpu.mac
+macro11.exe -ysl 32 -yus -m sysmac.sml -l _cpu.lst _cpu.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 
 echo.
 echo ===========================================================================
 echo Linking V1
 echo ===========================================================================
-..\..\php5\php.exe -c ..\..\php5\ -f ..\scripts\lst2bin.php _cpu.lst ./release/bolde3.bin bbk
+..\..\php5\php.exe -c ..\..\php5\ -f lst2bin.php _cpu.lst ./release/bolde3.bin bbk
 
 echo.
